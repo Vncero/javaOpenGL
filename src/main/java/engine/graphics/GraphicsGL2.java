ced75d0b785185c305b21a3b156e0482d6145c56
Vncero <vincentzheng1423@gmail.com>
2022-08-12 01:14:25 +0000

    minor edits on gl2

--- src/main/java/engine/graphics/GraphicsGL2.java
+++ src/main/java/engine/graphics/GraphicsGL2.java
@@ -1,6 +1,8 @@
 package engine.graphics;
-import com.jogamp.opengl.GL2;
+
 import org.jetbrains.annotations.NotNull;
+
+import com.jogamp.opengl.GL2;
 
 import static com.jogamp.opengl.GL2.*;
 
@@ -20,7 +22,7 @@ public class GraphicsGL2 {
         gl2.glClear(GL_COLOR_BUFFER_BIT);
     }
 
-    public static void fillRect(GL2 gl2, float x, float y, float width, float height) {
+    public static void fillRect(@NotNull GL2 gl2, float x, float y, float width, float height) {
         gl2.glTranslatef(x, y, 0);
         renderWrapper(
                 gl2,
@@ -37,19 +39,19 @@ public class GraphicsGL2 {
         gl2.glTranslatef(-x, -y, 0);
     }
 
-    public static void fillTriangle(GL2 gl2, float[] coords) {
+    public static void fillTriangle(@NotNull GL2 gl2) {
         renderWrapper(
             gl2,
             GL_TRIANGLES,
             () -> {
-                gl2.glVertex2f();
-                gl2.glVertex2f();
-                gl2.glVertex2f();
+                // gl2.glVertex2f();
+                // gl2.glVertex2f();
+                // gl2.glVertex2f();
             }
         );
     }
 
-    private static void renderWrapper(GL2 gl2, int glMode, Runnable r) {
+    private static void renderWrapper(@NotNull GL2 gl2, int glMode, Runnable r) {
         gl2.glRotatef((float) rotation, 0, 0, 1);
 
         gl2.glBegin(glMode);
